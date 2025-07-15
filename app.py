c23d848ab34fd46a62708005beaea845c98d62c3
PaiseDePange <Paisedepange@gmail.com>
2025-07-15 23:28:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def format_column_headers(headers):
     return unique
 
 def extract_table(df, start_label, col_count=11):
-    start_row = df[df.iloc[:, 0] == start_label].index[1]
+    start_row = df[df.iloc[:, 0] == start_label].index[0]
     header_row = start_row 
     headers_raw = df.iloc[header_row, 0:col_count].tolist()
     headers = format_column_headers(headers_raw)
