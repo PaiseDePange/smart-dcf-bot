0d81e1b5bd66beefd6a7bd5b66bd273aa4f4978c
PaiseDePange <Paisedepange@gmail.com>
2025-07-16 13:48:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,11 +89,11 @@ with tabs[0]:
     if uploaded_file and st.button("📥 Import Data"):
         df_all = pd.read_excel(uploaded_file, sheet_name="Data Sheet", header=None, engine="openpyxl")
         st.session_state["company_name"] = df_all.iloc[0, 1] if pd.notna(df_all.iloc[0, 1]) else "Unknown Company"
-        st.session_state["annual_pl"] = extract_table(df_all, "PROFIT & LOSS",2)
-        st.session_state["balance_sheet"] = extract_table(df_all, "BALANCE SHEET",2)
-        st.session_state["cashflow"] = extract_table(df_all, "CASH FLOW:",2)
-        st.session_state["quarterly"] = extract_table(df_all, "Quarters",2)
-        st.session_state["meta"] = extract_table(df_all, "META",1)
+        st.session_state["annual_pl"] = extract_table(df_all, "PROFIT & LOSS",2,11)
+        st.session_state["balance_sheet"] = extract_table(df_all, "BALANCE SHEET",2,11)
+        st.session_state["cashflow"] = extract_table(df_all, "CASH FLOW:",2,11)
+        st.session_state["quarterly"] = extract_table(df_all, "Quarters",2,11)
+        st.session_state["meta"] = extract_table(df_all, "META",1,3)
         st.session_state["data_imported"] = True
 
     if st.session_state.get("data_imported"):
