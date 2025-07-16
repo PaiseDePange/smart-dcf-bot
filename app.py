0c0499a5d3dea49749b7a2b3362d73f2a9873b39
PaiseDePange <Paisedepange@gmail.com>
2025-07-16 14:00:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ with tabs[0]:
         df_all = pd.read_excel(uploaded_file, sheet_name="Data Sheet", header=None, engine="openpyxl")
         st.session_state["company_name"] = df_all.iloc[0, 1] if pd.notna(df_all.iloc[0, 1]) else "Unknown Company"
         st.session_state["annual_pl"] = extract_table(df_all, "PROFIT & LOSS",1,11)
-        st.session_state["balance_sheet"] = extract_table(df_all, "BALANCE SHEET",2,11)
+        st.session_state["balance_sheet"] = extract_table(df_all, "BALANCE SHEET",1,11)
         st.session_state["cashflow"] = extract_table(df_all, "CASH FLOW:",1,11)
         st.session_state["quarterly"] = extract_table(df_all, "Quarters",1,11)
         st.session_state["meta"] = extract_table(df_all, "META",0,3)
